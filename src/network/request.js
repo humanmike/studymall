fffa31b0ef5b20142e65f2bdec9d24e292be197c
humanmike <43487411+humanmike@users.noreply.github.com>
2020-05-05 20:31:06 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // 封装网络请求
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000/api/wh',
+    baseURL: 'url',
     timeout: 5000
   })
 
